4615e2130a6e821c85fa39067ea20198e1e92588
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-08-16 17:44:25 +0700

    mvc-view,bootstrap

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// const express = require("express");
+import express from 'express';
+import engine from 'express-handlebars';
+import morgan from 'morgan';
+import path from 'path';
+const __dirname = path.resolve();
+
+// const morgan = require('morgan');
+
+const app = express();
+
+const port = 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+// Template engine
+app.engine('hbs', engine.engine({
+	extname:".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src/resources/views'));
+
+app.get("/", (req, res) => {
+	res.render('home');
+});
+app.get("/news", (req, res) => {
+	res.render('news');
+});
+
+app.listen(port, () => {
+	console.log(`Examplesssssss app listening on port ${port}`);
+});
